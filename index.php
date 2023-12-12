60243d54f5491625067a62c246ebba50b67e91a3
Miguel David Portillo Padiilla <mipor.278@gmail.com>
2023-12-12 14:15:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     session_start();
     require_once("funciones.php");
 
-    require('/vendor/autoload.php');
+    require __DIR__ . '/vendor/autoload.php';
     $envPath = './';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
